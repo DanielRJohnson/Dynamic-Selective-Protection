2b72b98fc417e42481c1061f05cf87dc51ea0f54
Daniel Johnson <danielrjohnson00@gmail.com>
2023-10-05 23:54:00 -0500

    qol io improvements

--- io_utils.py
+++ io_utils.py
@@ -1,12 +1,19 @@
 import os
 import numpy as np
 from scipy.io import loadmat, savemat
+from scipy.sparse import csr_matrix
 
 
-def load_matrices_from_dir(matdir: str, extract_func: callable, subset=None) -> dict[str, np.array]:
-    subset = subset if subset is not None else [f for f in os.listdir(matdir) if f.endswith(".mat")]
-    matfiles = [matdir + "/" + f for f in os.listdir(matdir) if f in subset]
-    matrices = {mf: extract_func(loadmat(mf)) for mf in matfiles}
+def load_matrices_from_dir(matdir: str, subset=None) -> dict[str, csr_matrix]:
+    extract_mode = matdir.split("/")[-1]  # name of folder
+    extractors = {  # how to get the sparse matrix from the object m
+        "raw": lambda m: m["Problem"]["A"][0][0],
+        "preconditioners": lambda m: m["L"],
+    }
+
+    subset = [m + (".mat" if extract_mode == "raw" else "_precond.mat") for m in subset]
+    matfiles = [matdir + "/" + f for f in os.listdir(matdir) if f in subset or subset is None]
+    matrices = {mf: extractors[extract_mode](loadmat(mf)) for mf in matfiles}
     return matrices
 
 
