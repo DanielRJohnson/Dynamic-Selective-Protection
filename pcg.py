ff3a7cdc47b035a1862211d07d0f0096671e1e3f
Daniel Johnson <danielrjohnson00@gmail.com>
2023-10-05 23:54:49 -0500

    change to julia wrapper

--- pcg.py
+++ pcg.py
@@ -1,65 +1,37 @@
-import numpy as np
-from numpy import ndarray
-from scipy.linalg import norm
-from scipy.sparse.linalg import spsolve_triangular
-from dataclasses import dataclass
+from scipy.sparse import find
+
+from juliacall import Main as jl
+from juliacall import Pkg as jlPkg
 
 
-@dataclass
-class Solution:
-    """ Output Solution object holding a solved x, final relres, and convergence_iteration """
-    x: ndarray
-    final_relres: float
-    final_iteration: int
-    converged: bool
+class Pcg:
+    """ Wrapper class that handles Python-Julia interop to Pcg package """
 
+    def __init__(self, A, b, M1, M2, tol, maxiter) -> None:
+        """ Initializes Julia packages and converts complex inputs to Julia types """
+        jlPkg.add("SparseArrays")
+        jl.seval("using SparseArrays")
+        jlPkg.activate("Pcg")
+        jl.seval("using Pcg")
+        self.b = self.__convert_numpy(b)
+        self.A, self.M1, self.M2 = [self.__convert_scipy(m) for m in [A, M1, M2]]
+        self.tol, self.maxiter = tol, maxiter
 
-def pcg(A: ndarray, b: ndarray, tol: float, maxit: int, M1: ndarray, M2: ndarray,
-        inject_error=False, error_pos=0, error_iter=0) -> Solution:
-    """
-    Solves a system of linear equations of the form Ax=b where the input matrix
-    A is symmetric, positive definite, and possibly very large and sparse.
-    https://en.wikipedia.org/wiki/Conjugate_gradient_method#The_preconditioned_conjugate_gradient_method
+    def pcg(self, inject_error=False, error_pos=0, error_iter=0) -> tuple:
+        """ Calls Pcg.pcg and wraps response in a Solution object """
+        return jl.pcg(self.A, self.b, self.M1, self.M2, self.tol,
+                      self.maxiter, inject_error, error_pos, error_iter)
 
-    :param A: Input matrix A
-    :param b: Output vector b
-    :param tol: tolerance of solution to break early if |r|/|b| < tol
-    :param maxit: maximum number of iterations for the solver to make
-    :param M1: M1 factor of the preconditioner matrix M s.t. M1M2 = M
-    :param M2: M2 factor of the preconditioner matrix M s.t. M2M2 = M
-    :param inject_error: whether or not to inject an error
-    :param error_pos: The position of p to inject the error
-    :param error_iter: The iteration in which to inject the error
-    :return: Solution
-    """
-    N = A.shape[0]
-    x = np.zeros((N, 1))  # Nx1
-    r = b - A @ x  # Nx1
+    def __call__(self, *args, **kwargs):
+        """ Call shorthand to pcg method """
+        return self.pcg(*args, **kwargs)
 
-    z = spsolve_triangular(M2, spsolve_triangular(M1, r), lower=False)  # Nx1  (z = M^(-1)r => z = (M2\(M1\r))
-    p = z  # Nx1
+    def __convert_scipy(self, A):
+        """ Converts a scipy sparse matrix to a Julia SparseMatrixCSC """
+        I, J, V = map(lambda v: jl.Vector(v), find(A))
+        inc = jl.seval("x -> x .+ 1")  # change to 1-indexed
+        return jl.sparse(inc(I), inc(J), V)
 
-    norm_r, norm_b = norm(r), norm(b)
-    relres = norm_r / norm_b
-    i = 0
-
-    for i in range(maxit):
-        if relres < tol:
-            break
-
-        if inject_error and i == error_iter:
-            p[error_pos] = p[error_pos] + p.max()
-
-        q = A @ p  # Nx1
-        v = z.T @ r  # 1x1
-        alpha = v / (p.T @ q)  # 1x1
-        x += alpha * p  # Nx1
-        r -= alpha * q  # Nx1
-        z = spsolve_triangular(M2, spsolve_triangular(M1, r), lower=False)  # Nx1
-        beta = (z.T @ r) / v  # 1x1
-        p = z + beta * p  # Nx1
-
-        norm_r = norm(r)
-        relres = norm_r / norm_b
-
-    return Solution(x, relres, i, relres <= tol)
+    def __convert_numpy(self, x):
+        """ Converts a numpy array to a Julia Matrix """
+        return jl.Matrix(x)
